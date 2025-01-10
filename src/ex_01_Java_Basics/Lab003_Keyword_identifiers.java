24af82910475723326bde6befee7f6028a1e2bf5
Vedavathi <vedavr@gmail.com>
2025-01-11 02:54:11 +0530

    11Jankeywords

---
+++ src/ex_01_Java_Basics/Lab003_Keyword_identifiers.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Keyword_identifiers {
+    public static void main(String[] args) {
+        System.out.println("in this class keywords are package,public");
+    }
+}
